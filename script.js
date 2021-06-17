45ab375c336be674cd4e17f44f9e875a775f35ca
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-06-16 23:24:03 -0300

    requisito bonus 20

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function btnClick() {
 const clickButton = document.getElementById('submit');
 clickButton.addEventListener('click', btnClick);
 
+// Função de conferir se o checkbox foi checkado
 function checkedBox() {
   const button = document.getElementById('submit-btn');
   const checkBox = document.getElementById('agreement');
@@ -22,5 +23,17 @@ function checkedBox() {
   }
 }
 
+// Evento de habilitar o botão quando o checkbox for checado
 const sendForm = document.getElementById('agreement');
 sendForm.addEventListener('click', checkedBox);
+
+// Função para contabilizar o número de caracteres na textarea
+function countText() {
+  const check = document.getElementById('textarea').value;
+  const total = 500 - check.length;
+  document.getElementById('counter').innerHTML = total;
+}
+
+// Evento para atualizar o contador
+const refreshCounter = document.getElementById('textarea');
+refreshCounter.addEventListener('keyup', countText);
